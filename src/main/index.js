06b3b2f8ee484dcb74245a142ea50573e2502766
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-07-13 18:02:16 +0000

    The TypeScript LSP killed the mood.

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,7 @@
 "use strict";
 
-console.info(`Welcome to Lightingale Hyacinth!`);
+let b64FromBytes = (b64Data) => {
+	let dataString = atob(b64Data.replaceAll('-', '+').replaceAll('_', '/'));
+	let u8Buf = new Uint8Array(dataString.length);
+	for (let i = 0; i < u8Buf.length;
+};
